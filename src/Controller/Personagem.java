11fb8bd23a1df19a91f3429389fc2fcd7335a073
DreaMagici4n <lucascostamr812@gmail.com>
2023-05-18 17:15:17 -0300

    feat: pastas seguindo modelo MVC

--- src/Controller/Personagem.java
+++ src/Controller/Personagem.java
@@ -1,3 +1,4 @@
+package src.Controller;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -64,7 +65,5 @@ public void movePlayer(String input, Worlds worlds) {
 
         worlds.updateWorld(playerX, playerY, '\u2620');
     }
-
-
 }
 }
--- src/Controller/ZeldaGame.java
+++ src/Controller/ZeldaGame.java
@@ -1,3 +1,4 @@
+package src.Controller;
 import java.util .Scanner;
 
 public class ZeldaGame {
