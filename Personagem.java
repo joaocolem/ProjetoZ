6038da17e8398dbd2c4559a7c607b2a1cbd6eb2b
joaocolem <105292741+joaocolem@users.noreply.github.com>
2023-05-18 16:24:32 -0300

    Primeiros passos

---
+++ Personagem.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author joao_
+ */
+public class Personagem {
+    private int playerX;
+    private int playerY;
+
+    public Personagem(int x, int y) {
+        this.playerX = x;
+        this.playerY = y;
+    }
+
+    public int getPlayerX() {
+        return playerX;
+    }
+
+    public int getPlayerY() {
+        return playerY;
+    }
+
+    public void setPlayerX(int x) {
+        playerX = x;
+    }
+
+    public void setPlayerY(int y) {
+        playerY = y;
+    }
+
+public void movePlayer(String input, Worlds worlds) {
+    int newX = playerX;
+    int newY = playerY;
+
+    if (input.equals("w")) {
+        newY = playerY - 1;
+    } else if (input.equals("a")) {
+        newX = playerX - 1;
+    } else if (input.equals("s")) {
+        newY = playerY + 1;
+    } else if (input.equals("d")) {
+        newX = playerX + 1;
+    }
+
+    if (worlds.isValidMove(newX, newY)) {
+        worlds.updateWorld(playerX, playerY, '.');
+
+        if (worlds.getCurrentWorld() == 1 && newX == 0 && newY == 2) {
+            worlds.setCurrentWorld(2);
+            playerX = 5;
+            playerY = 3;
+        } else if (worlds.getCurrentWorld() == 2 && newX == 6 && newY == 3) {
+            worlds.setCurrentWorld(1);
+            playerX = 1;
+            playerY = 2;
+        } else {
+            playerX = newX;
+            playerY = newY;
+        }
+
+        worlds.updateWorld(playerX, playerY, '\u2620');
+    }
+
+
+}
+}
