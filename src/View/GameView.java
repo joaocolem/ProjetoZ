01c0552b1927ee14b5ec459466a22bbee02b8455
DreaMagici4n <lucascostamr812@gmail.com>
2023-05-21 15:46:03 -0300

    feat: KeyListener implementado

--- src/View/GameView.java
+++ src/View/GameView.java
@@ -3,6 +3,7 @@ package src.View;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
 import src.Controller.Personagem;
@@ -10,32 +11,62 @@ import src.Controller.Worlds;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GameView {
     public static Personagem personagem = new Personagem(2, 2);
     public static Worlds worlds = new Worlds();
+    public static JPanel panel = new JPanel();
+    public static JFrame frame = new JFrame("Swing Example");
+    public static JLabel[][] labels;
 
     public static void main(String[] args) {
 
         if (worlds.getCurrentWorld() == 1) {
-            displayWorld(worlds.getWorld1());
+            displayMundo(worlds.getWorld1());
         } else if (worlds.getCurrentWorld() == 2) {
-            displayWorld(worlds.getWorld2());
+            displayMundo(worlds.getWorld2());
         } else if (worlds.getCurrentWorld() == 3) {
-            displayWorld(worlds.getWorld3());
+            displayMundo(worlds.getWorld3());
         }
     }
 
-    public static void displayWorld(char[][] world) {
-        String caracter;
+    public static void displayMundo (char[][] world){
 
         // Criando instacia JFrame
-        JFrame frame = new JFrame("Swing Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //JPanel
-        JPanel panel = new JPanel();
         panel.setBackground(Color.BLACK);
+        panel.setFocusable(true);
+        panel.requestFocusInWindow();
+
+        // move o personagem
+        panel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+              if (e.getKeyCode() == KeyEvent.VK_W) {
+                personagem.movePlayer("w", worlds);
+                atualizarMundo(world);
+              }
+              else if (e.getKeyCode() == KeyEvent.VK_A){
+                personagem.movePlayer("a", worlds);
+                atualizarMundo(world);
+              }
+              else if (e.getKeyCode() == KeyEvent.VK_S){
+                personagem.movePlayer("s", worlds);
+                atualizarMundo(world);
+              }
+              else if (e.getKeyCode() == KeyEvent.VK_D){
+                personagem.movePlayer("d", worlds);
+                atualizarMundo(world);
+              }
+            }
+        });
+
+        gerarMundo(world);
 
         //Adicionando Panel ao Frame
         frame.setContentPane(panel);
@@ -43,7 +74,16 @@ public class GameView {
         //Tamanho da janela
         frame.setPreferredSize(new Dimension(190, 300));
 
-        JLabel[][] labels = new JLabel[world.length][world[0].length];
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+
+    public static void gerarMundo(char[][] world ){
+        String caracter;
+        labels = new JLabel[world.length][world[0].length];
+        
 
         for (int i = 0; i < world.length; i++) {
             for (int j = 0; j < world[0].length; j++) {
@@ -62,8 +102,15 @@ public class GameView {
                 panel.add(labels[i][j]);//adicionando label ao panel
             }
         }
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    }
+
+    public static void atualizarMundo(char [][] world) {
+        panel.removeAll();
+        // Recriar o mundo
+        gerarMundo(world);
+
+        // Atualizar o JPanel
+        panel.revalidate();
+        panel.repaint();
     }
 }
