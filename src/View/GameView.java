84cbcc7ea0dfd842a4bcf057186ddfd365af8630
DreaMagici4n <lucascostamr812@gmail.com>
2023-05-27 14:32:35 -0300

    refactor: retirado funcao atualizarMundos e mudanca no nome do gerarMundo para mountWorld

--- src/View/GameView.java
+++ src/View/GameView.java
@@ -7,7 +7,6 @@ import javax.swing.border.EmptyBorder;
 
 import src.Controller.Personagem;
 import src.Controller.World;
-import src.Controller.Worlds;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -28,11 +27,11 @@ public class GameView {
         displayMundo();
     }
 
-    public static void displayMundo (){
+    public static void displayMundo() {
         // Criando instacia JFrame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        //JPanel
+
+        // JPanel
         panel.setBackground(Color.BLACK); // cor de fundo do Jpanel
         panel.setFocusable(true);
         panel.requestFocusInWindow();
@@ -43,33 +42,30 @@ public class GameView {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_W) {
                     currentWorld = personagem.movePlayer("w", currentWorld);
-                    atualizarMundo(currentWorld.getMap());
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_A){
-                    currentWorld= personagem.movePlayer("a", currentWorld);
-                    atualizarMundo(currentWorld.getMap());
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_S){
-                    currentWorld= personagem.movePlayer("s", currentWorld);
-                    atualizarMundo(currentWorld.getMap());
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_D){
-                    currentWorld= personagem.movePlayer("d", currentWorld);
-                    atualizarMundo(currentWorld.getMap());
+                    mountDisplay(currentWorld.getMap());
+                } else if (e.getKeyCode() == KeyEvent.VK_A) {
+                    currentWorld = personagem.movePlayer("a", currentWorld);
+                    mountDisplay(currentWorld.getMap());
+                } else if (e.getKeyCode() == KeyEvent.VK_S) {
+                    currentWorld = personagem.movePlayer("s", currentWorld);
+                    mountDisplay(currentWorld.getMap());
+                } else if (e.getKeyCode() == KeyEvent.VK_D) {
+                    currentWorld = personagem.movePlayer("d", currentWorld);
+                    mountDisplay(currentWorld.getMap());
+                } else if (e.getKeyCode() == KeyEvent.VK_I) {
+                    // atualizaMundo();
                 }
             }
         });
 
-        gerarMundo(currentWorld.getMap());// cria o mundo pela primeira vez
+        mountDisplay(currentWorld.getMap());// cria o mundo pela primeira vez
 
-        // panel.setLayout(new GridLayout(8, 28)); // Adjust the dimensions based on your text
-         //Adicionando Panel ao Frame
+        // Adicionando Panel ao Frame
         frame.setContentPane(panel);
 
-        //Tamanho da janel
+        // Tamanho da janel
         frame.setLayout(new GridLayout(8, 28));
         frame.setPreferredSize(new Dimension(800, 300));
-
 
         frame.pack();
         frame.setLocationRelativeTo(null);
@@ -77,40 +73,35 @@ public class GameView {
 
     }
 
-    public static void gerarMundo(char[][] world ){
+    public static void mountDisplay(char[][] world) {
         String caracter;
 
         JLabel[][] gameLabels = new JLabel[world.length][world[0].length];
 
+        panel.removeAll();
 
         for (int i = 0; i < world.length; i++) {
             for (int j = 0; j < world[0].length; j++) {
 
-                caracter = i == personagem.getPlayerY() && j == personagem.getPlayerX() ? "P" : String.valueOf(world[i][j]); //Posiciona o personagem
+                caracter = i == personagem.getPlayerY() && j == personagem.getPlayerX() ? "P"
+                        : String.valueOf(world[i][j]); // Posiciona o personagem
 
-                gameLabels[i][j] = new JLabel(caracter);//setando valor da label(cada caracter)
+                gameLabels[i][j] = new JLabel(caracter);// setando valor da label(cada caracter)
 
                 if (caracter.equals(".")) {
-                    gameLabels[i][j].setBorder(new EmptyBorder(0, 6, 0, 10)); // o '.' ocupa menos espaco que '#', por isso e necessario aumentar a sua largura
+                    gameLabels[i][j].setBorder(new EmptyBorder(0, 6, 0, 10)); // o '.' ocupa menos espaco que '#', por
+                                                                              // isso e necessario aumentar a sua
+                                                                              // largura
                 } else {
-                    gameLabels[i][j].setBorder(new EmptyBorder(0, 1, 0, 10));//coordenadas do caracter
+                    gameLabels[i][j].setBorder(new EmptyBorder(0, 1, 0, 10));// coordenadas do caracter
                 }
 
-                gameLabels[i][j].setForeground(Color.WHITE); //cor da letra
-                panel.add(gameLabels[i][j]);//adicionando label ao panel
+                gameLabels[i][j].setForeground(Color.WHITE); // cor da letra
+                panel.add(gameLabels[i][j]);// adicionando label ao panel
             }
         }
-    }
 
-    public static void atualizarMundo(char [][] world) {
-        panel.removeAll();
-        // panel.setLayout(new GridLayout(8, 28));
-
-        // Recriar o mundo
-        gerarMundo(world);
-        // Atualizar o JPanel
         panel.revalidate();
         panel.repaint();
-
     }
 }
