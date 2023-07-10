4b02c6f017f496dceb0dedf0d539ecbc31935ba8
joaocolem <105292741+joaocolem@users.noreply.github.com>
2023-07-10 20:28:24 -0300

    mudancas setting.json

---
+++ src/testes/Calculator.java
@@ -0,0 +1,7 @@
+package src.testes;
+
+public class Calculator {
+    public int add(int a, int b){
+        return a +b;
+    }
+}
---
+++ src/testes/CalculatorTest.java
@@ -0,0 +1,14 @@
+package src.testes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void sum(){
+        assertEquals(2, 2);
+    }
+}
